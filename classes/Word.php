74d8f0e03d6b951062016553a89c79912ee9308e
JensRosseel <slaythehorizon.games@gmail.com>
2021-08-09 12:06:46 +0200

    Fix message when you reach 10 right or wrong answers

--- classes/Word.php
+++ classes/Word.php
@@ -4,12 +4,12 @@ class Word
 {
     public function verify(string $answer, string $guess)
     {
-        if($guess == $answer)
+        if(strtolower($guess) == $answer)
         {
             $_SESSION['win']++;
-            if($_SESSION['win'] == 10 or $_SESSION['loss'] == 10)
+            if($_SESSION['win'] == 10 || $_SESSION['loss'] == 10)
             {
-                echo "<script> alert('Congratulation you have reached the end! \n Your score is {$_SESSION['win']}/{$_SESSION['loss']}'); </script>";
+                echo "<script> alert('Congratulation you have reached the end! \\n Your score is {$_SESSION['win']}/{$_SESSION['loss']}'); </script>";
             }
             else
             {
@@ -19,16 +19,14 @@ class Word
         else
         {
             $_SESSION['loss']++;
-            if($_SESSION['win'] == 10 or $_SESSION['loss'] == 10)
+            if($_SESSION['win'] == 10 || $_SESSION['loss'] == 10)
             {
-                echo "<script> alert('Congratulation you have reached the end! \n Your score is {$_SESSION['win']}/{$_SESSION['loss']}'); </script>";
+                echo "<script> alert('Congratulation you have reached the end! \\n Your score is {$_SESSION['win']}/{$_SESSION['loss']}'); </script>";
             }
             else
             {
                 echo "<script> alert('Your guess is NOT correct!!!'); </script>";
             }   
         }
-        // Bonus: allow answers with different casing (example: both bread or Bread can be correct answers, even though technically it's a different string)
-        // Bonus (hard): can you allow answers with small typo's (max one character different)?
     }
 }
